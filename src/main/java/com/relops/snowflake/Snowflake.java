b90491aa1c1b5c32580b729d9c737134e85ed498
Xiaojing CHEN <chxj1992@gmail.com>
2017-10-27 21:11:13 -0500

    Update Snowflake.java

--- src/main/java/com/relops/snowflake/Snowflake.java
+++ src/main/java/com/relops/snowflake/Snowflake.java
@@ -1,64 +1,69 @@
-package com.relops.snowflake;
+package name.chxj1992;
 
 /**
- * A snowflake is a source of k-ordered unique 64-bit integers.
+ * A Snowflake like ID generator
  */
 public class Snowflake {
 
-  public static final int NODE_SHIFT = 10;
-  public static final int SEQ_SHIFT = 12;
+    //  private static final int NODE_SHIFT = 10;
+    private static final int NODE_SHIFT = 8;
+    private static final int SEQ_SHIFT = 12;
 
-  public static final short MAX_NODE = 1024;
-  public static final short MAX_SEQUENCE = 4096;
+    private static final short MAX_NODE = 256;
+    private static final short MAX_SEQUENCE = 4096;
 
-  private short sequence;
-  private long referenceTime;
+    private short sequence;
+    private long referenceTime;
 
-  private int node;
+    private int node;
 
-  /**
-   * A snowflake is designed to operate as a singleton instance within the context of a node.
-   * If you deploy different nodes, supplying a unique node id will guarantee the uniqueness
-   * of ids generated concurrently on different nodes.
-   *
-   * @param node This is an id you use to differentiate different nodes.
-   */
-  public Snowflake(int node) {
-    if (node < 0 || node > MAX_NODE) {
-      throw new IllegalArgumentException(String.format("node must be between %s and %s", 0, MAX_NODE));
-    }
-    this.node = node;
-  }
-
-  /**
-   * Generates a k-ordered unique 64-bit integer. Subsequent invocations of this method will produce
-   * increasing integer values.
-   *
-   * @return The next 64-bit integer.
-   */
-  public long next() {
-
-    long currentTime = System.currentTimeMillis();
-    long counter;
-
-    synchronized(this) {
-
-      if (currentTime < referenceTime) {
-        throw new RuntimeException(String.format("Last referenceTime %s is after reference time %s", referenceTime, currentTime));
-      } else if (currentTime > referenceTime) {
-        this.sequence = 0;
-      } else {
-        if (this.sequence < Snowflake.MAX_SEQUENCE) {
-          this.sequence++;
-        } else {
-          throw new RuntimeException("Sequence exhausted at " + this.sequence);
+    public Snowflake(int node) {
+        if (node < 0 || node > MAX_NODE) {
+            throw new IllegalArgumentException(String.format("node must be between %s and %s", 0, MAX_NODE));
         }
-      }
-      counter = this.sequence;
-      referenceTime = currentTime;
+        this.node = node;
     }
 
-    return currentTime << NODE_SHIFT << SEQ_SHIFT | node << SEQ_SHIFT | counter;
-  }
+    public Snowflake() {}
+
+    public void setNode(int node) {
+
+        if (node < 0 || node > MAX_NODE) {
+            throw new IllegalArgumentException(String.format("node must be between %s and %s", 0, MAX_NODE));
+        }
+        this.node = node;
+    }
+
+    synchronized public long next() {
+
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime > referenceTime) {
+            sequence = 1;
+        } else {
+            if (sequence < Snowflake.MAX_SEQUENCE) {
+                sequence++;
+            } else {
+                currentTime = waitNextMill();
+                sequence = 0;
+            }
+        }
+        referenceTime = currentTime;
+
+        return currentTime << NODE_SHIFT << SEQ_SHIFT | node << SEQ_SHIFT | sequence;
+    }
+
+    private long waitNextMill() {
+        long cur;
+        do {
+            cur = System.currentTimeMillis();
+        } while (cur <= referenceTime);
+        return cur;
+    }
+
+    private static void main(String[] args){
+
+        new Snowflake(1).next();
+    }
 
 }
